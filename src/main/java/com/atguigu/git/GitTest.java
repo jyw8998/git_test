ccb5a732be94ae89bc7dbe132b0985b8e5eaa0b9
jinyi <jinyi@qq.com>
2022-02-14 15:24:40 +0800

    first

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author jywang
+ * @create 2022-02-14 15:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
